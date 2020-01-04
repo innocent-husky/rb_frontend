63b3e762ae90eeb3b3d1b56057c7d5efe8ef6e9b
inno <1597076708@qq.com>
2020-01-04 23:00:56 +0800

    制作header组件

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import {Button} from 'antd'
+import Header from "../components/Header";
 
 const Home = () => (
     <div>
@@ -8,6 +9,7 @@ const Home = () => (
             <title>Home</title>
             <link rel="icon" href="/favicon.ico"/>
         </Head>
+        <Header/>
         <div style={{display:"flex",justifyContent:"center",padding:"4px"}}><Button>我是按钮</Button></div>
     </div>
 )
